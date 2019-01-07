ce36055fb2c29636829b68a5921c06572120d8ea
Alex McKenna <alexmckenna@outlook.com>
2019-01-07 14:24:40 +0000

    Add tracing solver
    A tracing solver has been added, which wraps another solver and
    uses Debug.Trace to output the result of each call to isOptimal,
    toResult and step. This can be used to debug an implementation of
    a solver.

---
+++ src/Numeric/Optimization/Trace.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE ConstraintKinds        #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE UndecidableInstances   #-}
+
+module Numeric.Optimization.Trace where
+
+import Debug.Trace
+
+import Numeric.Optimization.Solver.Class
+
+
+type IsTrace a =
+    (Solver a, Show a, Show (Stop a), Show (Vars a))
+
+
+newtype Trace a = Trace { getSolver :: a }
+    deriving (Show)
+
+
+instance (IsTrace a) => Solver (Trace a) where
+    type CanSolve (Trace a) = CanSolve a
+    type Stop     (Trace a) = Stop a
+    type Vars     (Trace a) = Vars a
+
+    isOptimal   =
+        trace "isOptimal = " . traceShowId . isOptimal . getSolver
+
+    toResult    =
+        trace "toResult = " . traceShowId . toResult . getSolver
+
+    step        =
+        trace "step = " . traceShowId . fmap Trace . step . getSolver
+
+
+traceSolver :: (IsTrace a) => a -> Trace a
+traceSolver = Trace
+
